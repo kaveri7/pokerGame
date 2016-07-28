c3562bb9ee96024729fc7a4fa45fd6bf5ac82fb2
chiragrtr <chiragrtr@gmail.com>
2016-07-28 12:43:46 +0530

    added compareTo method

--- Card.java
+++ Card.java
@@ -1,14 +1,42 @@
-class Card {
-    String suit;            //can have value such as club, diamond,
-    int pip;
-    public String toString(){
-        return "" + pip + suit;
-    }
+class Card implements Comparable{
+    private String suit;            //can have value such as club, diamond,
+    private int pip;
+
     public Card(String suit, int pip){
         this.suit = suit;
         this.pip = pip;
     }
-    public String compare(Card anotherCard){
-        return "Greater";
+    public String getColor(){
+        if(suit == "spade" || suit == "club") return "black";
+        return "red";
+    }
+    public String getSuit(){
+        return suit;
+    }
+    public String getPip(){
+        if(pip == 1) return "A";
+        if(pip == 10) return "T";
+        if(pip == 11) return "J";
+        if(pip == 12) return "Q";
+        if(pip == 13) return "K";
+        return "" + pip;
+    }
+    @Override
+    public int compareTo(Object anotherCard){
+        if(this.pip > ((Card)anotherCard).pip) return 1;
+        if(this.pip < ((Card)anotherCard).pip) return -1;
+        return 0;
+    }
+    @Override
+    public String toString(){
+        return "" + pip + suit;
+    }
+    public String compareSuit(Card anotherCard){
+        if(anotherCard.suit == this.suit) return "same";
+        return "different";
+    }
+    public String compareColor(Card anotherCard){
+        if(anotherCard.getColor() == this.getColor()) return "same";
+        return "different";
     }
 }
